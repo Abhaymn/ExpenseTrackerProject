0ab31467bece31005829f887d6c4d4ff529881e4
abhaymn <abhaykrishnatsy007>
2023-02-01 15:47:27 +0530

    logout

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // import { useContext } from "react";
-import { Container, Nav, Navbar,NavLink } from "react-bootstrap";
+import { Button, Container, Nav, Navbar,NavLink } from "react-bootstrap";
 import { Link, Routes,Route ,BrowserRouter} from "react-router-dom";
 import AuthPage from "../pages/AuthPage";
 import Main from "../pages/main";
@@ -10,7 +10,7 @@ import LoginMessage from "../UserProfile/LoginMessage";
 // import AuthContext from "../Store/Auth-Context";
 
 const Header=(props)=>{
-
+  
    
     // const authCtx= useContext(AuthContext);
     // const isLoggedIn=authCtx.isLoggedIn;
@@ -23,6 +23,7 @@ const Header=(props)=>{
                         <NavLink as={Link} to="/login">login</NavLink>
                         {/* <NavLink as={Link} to='/main'>main</NavLink> */}
                     </Nav>
+                    <Button as={Link} to="/login">LogOut</Button>
                 </Container>
             </Navbar>
 
@@ -31,6 +32,7 @@ const Header=(props)=>{
                
                 <Route path='/main' element={<Main/>}/>
                 <Route path='/resetpassword' element={<LoginMessage/>}/>
+
                 
                
             </Routes>

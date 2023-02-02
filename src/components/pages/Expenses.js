b064d93ba711c3baaf1339a3791978077873b7f3
abhaymn <abhaykrishnatsy007>
2023-02-02 13:28:11 +0530

    config using fiewbase

--- src/components/pages/Expenses.js
+++ src/components/pages/Expenses.js
@@ -1,28 +1,100 @@
-import React,{useRef} from "react";
-import classes from "./Expenses.module.css";
-import { useState } from "react";
-const Expenses=()=>{
+import React, { useRef, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-    const amountRef = useRef();
-    const typeRef = useRef();
-    const descriptionRef = useRef();
+import classes from './Expenses.module.css';
+import ExpenseItems from '../ExpenseItems';
+import { expenseAction } from '../Store/expenseSlice';
+import { addingExpense } from '../Store/expense-actions';
+import { requestingExpense } from '../Store/expense-actions';
 
-    const [expenses, setExpenses] = useState([]);
 
-    const addExpenseHandler = async (event) => {
-        event.preventDefault();
-    
-        const inputData = {
-          amount: amountRef.current.value,
-          type: typeRef.current.value,
-          description: descriptionRef.current.value,
-        };
-        setExpenses([...expenses, inputData]);
+const Expenses = () => {
+  const amountRef = useRef();
+  const typeRef = useRef();
+  const descriptionRef = useRef();
+  const dispatch = useDispatch();
+  const firstTime = useSelector((state) => state.expense.firstTime);
+  const expenseList = useSelector((state) => state.expense.expenses);
+  const totalAmount = useSelector((state) => state.expense.totalAmount);
+
+  const email = JSON.parse(localStorage.getItem('idToken')).email;
+  const emailUrl = email.replace(/[@.]/g, '');
+
+  // adding new expenses
+  const removeInputData = () => {
+    amountRef.current.value = '';
+    typeRef.current.value = '';
+    descriptionRef.current.value = '';
+  };
+
+  const addExpenseHandler = async (event) => {
+    event.preventDefault();
+
+    const inputData = {
+      amount: amountRef.current.value,
+      type: typeRef.current.value,
+      description: descriptionRef.current.value,
+    };
+
+    dispatch(addingExpense(inputData, emailUrl, removeInputData));
+  };
+
+  // showing expenses when page is refreshed
+  useEffect(() => {
+    if (firstTime) {
+      dispatch(requestingExpense(emailUrl));
+      console.log(emailUrl);
     }
+  }, [emailUrl, dispatch, firstTime]);
 
-    return(
-        <div>
-        <form className={classes.form} onSubmit={addExpenseHandler}>
+  // editing the expense
+  const edit = (item) => {
+    const updatedAmount = totalAmount - Number(item.amount);
+    const updatedExpense = expenseList.filter(
+      (expense) => expense.id !== item.id
+    );
+    amountRef.current.value = item.amount;
+    typeRef.current.value = item.type;
+    descriptionRef.current.value = item.description;
+
+    dispatch(
+      expenseAction.removeExpense({
+        expenses: updatedExpense,
+        totalAmount: updatedAmount,
+      })
+    );
+  };
+
+  // deleting the expense
+  const deleted = (item) => {
+    const updatedAmount = totalAmount - Number(item.amount);
+    const updatedExpense = expenseList.filter(
+      (expense) => expense.id !== item.id
+    );
+    dispatch(
+      expenseAction.removeExpense({
+        expenses: updatedExpense,
+        totalAmount: updatedAmount,
+      })
+    );
+  };
+
+  // mapping the expenses
+  const newExpenseList = expenseList.map((item) => (
+    <ExpenseItems
+      item={item}
+      key={item.id}
+      edit={edit}
+      deleted={deleted}
+      emailUrl={emailUrl}
+    />
+  ));
+
+  // Expense Component
+  return (
+    <React.Fragment>
+      
+      <form className={classes.form} onSubmit={addExpenseHandler}>
         <div className={classes.type}>
           <label>Expense Category: </label>
           <select ref={typeRef} required>
@@ -45,15 +117,19 @@ const Expenses=()=>{
           <button type='submit'>Add Expense</button>
         </div>
       </form>
-        <ul className={classes.items}>
-        {expenses.map((expense, index) => (
-            <li key={index} >
-            Amount: {expense.amount}, Type: {expense.type},
-            Description: {expense.description}
-          </li>
-            ))}
-        </ul>
-      </div>
-    )
-}
+      {expenseList.length > 0 && (
+        <div className={classes.items}>
+          <div className={classes.title}>
+            <span className={classes.titletype}>Type</span>
+            <span className={classes.titleamount}>Amount</span>
+            <span className={classes.titledescription}>Description</span>
+            <span className={classes.total}>Total = Rs.{totalAmount}</span>
+          </div>
+          {newExpenseList}
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
+
 export default Expenses;
